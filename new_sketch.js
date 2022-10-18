ca93ee23978176cd7d2d9b9f3c12f0160a859614
Neel Shivdasani <neel.shivdasani@gmail.com>
2022-10-17 21:34:52 -0400

    only jfa once

--- new_sketch.js
+++ new_sketch.js
@@ -18,19 +18,106 @@ uniform vec2 res;
 uniform float time;
 uniform sampler2D jfa;
 
+
+vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
+
+float snoise(vec3 v){ 
+  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+// First corner
+  vec3 i  = floor(v + dot(v, C.yyy) );
+  vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+// Other corners
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min( g.xyz, l.zxy );
+  vec3 i2 = max( g.xyz, l.zxy );
+
+  //  x0 = x0 - 0. + 0.0 * C 
+  vec3 x1 = x0 - i1 + 1.0 * C.xxx;
+  vec3 x2 = x0 - i2 + 2.0 * C.xxx;
+  vec3 x3 = x0 - 1. + 3.0 * C.xxx;
+
+// Permutations
+  i = mod(i, 289.0 ); 
+  vec4 p = permute( permute( permute( 
+             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+// Gradients
+// ( N*N points uniformly over a square, mapped onto an octahedron.)
+  float n_ = 1.0/7.0; // N=7
+  vec3  ns = n_ * D.wyz - D.xzx;
+
+  vec4 j = p - 49.0 * floor(p * ns.z *ns.z);  //  mod(p,N*N)
+
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+  vec4 x = x_ *ns.x + ns.yyyy;
+  vec4 y = y_ *ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+
+  vec4 b0 = vec4( x.xy, y.xy );
+  vec4 b1 = vec4( x.zw, y.zw );
+
+  vec4 s0 = floor(b0)*2.0 + 1.0;
+  vec4 s1 = floor(b1)*2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+
+  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+  vec3 p0 = vec3(a0.xy,h.x);
+  vec3 p1 = vec3(a0.zw,h.y);
+  vec3 p2 = vec3(a1.xy,h.z);
+  vec3 p3 = vec3(a1.zw,h.w);
+
+//Normalise gradients
+  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+
+// Mix final noise value
+  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  m = m * m;
+  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+                                dot(p2,x2), dot(p3,x3) ) );
+}
+
 void main() {
     vec2 uv = gl_FragCoord.xy/res;
     vec4 t = texture(jfa,uv);
 
+    float n1 = snoise(vec3(uv*100.,time*.2));
     float d = distance(vec2(t.x,t.y),uv);
-    float d2 = distance(vec2(t.b,t.a),uv);
-    float dist = sin(d*100.+time);
-    float dist2 = sin(d2*100.-time);
+    // float d2 = distance(vec2(t.b,t.a),uv);
+    float d2 = distance(vec2(t.x,t.y),uv);
+    float dist = (sin(d*100.+time*1.)+1.)*.75 * (step(.02,d));
+    float dist2 = (sin(d2*100.-time*1.)+1.)*.65 * step(.02,d2);
+    dist = smoothstep(.45,.55,dist);
+    dist2 = smoothstep(.45,.55,dist2);
+    dist += step(.8,snoise(vec3(uv*100.,time*.5)))*.4;
+    dist2 += step(.8,snoise(vec3(uv*100.,-time*.5)))*.4;
+
+    vec4 c1 = vec4(5, 25, 35,255.)/255.;
+    vec4 c2 = vec4(0, 100, 148,255.)/255.;
+    vec4 c3 = vec4(240, 211, 247,255.)/255.;
+    vec4 c4 = vec4(231, 90, 124,255.)/255.;
+    
+    vec4 ca = mix(c1,c2,dist);
+    vec4 cb = mix(c3,c4,dist);
+    cc = mix(ca,cb,dist2);
 
 
 
-    // cc *=vec4(t.r,1.-t.r,t.g,1.);
-    cc = vec4(dist,dist,dist2,dist2);
+    // cc = vec4(dist,dist,dist2,1.);
     // cc.a = 1.;
 }`;
 //fragment shader for init
@@ -192,6 +279,54 @@ function main() {
     gl.activeTexture(gl.TEXTURE0+0);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, glyphImage);
     gl.generateMipmap(gl.TEXTURE_2D);
+
+
+
+        //ping
+        gl.bindFramebuffer(gl.FRAMEBUFFER, ping);
+        
+        gl.viewport(0,0,resx,resy);
+        gl.clearColor(0, 0, 1, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.useProgram(initProgram);
+        gl.uniform1i(loc_glyph_init,0);
+        
+        gl.bindVertexArray(vao_ping);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        firstframe && console.log(resx);
+        let steps = Math.ceil(Math.log2(resx));
+        firstframe && console.log("steps: "+steps);
+         steps = Math.min(steps,steps_override);
+			let frame=0
+        for (let i = 0; i <steps; i++) {
+            //pong
+            gl.bindFramebuffer(gl.FRAMEBUFFER,pong);
+            gl.viewport(0,0,resx,resy);
+            gl.clearColor(0, 0, 1, 1);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            gl.useProgram(pongProgram);
+            gl.uniform1i(loc_ping_pong,1);
+            gl.bindVertexArray(vao_pong);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+            //ping
+            gl.bindFramebuffer(gl.FRAMEBUFFER,ping);
+            gl.viewport(0,0,resx,resy);
+            gl.clearColor(0, 0, 1, 1);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            gl.useProgram(pingProgram);
+            gl.uniform1i(loc_pong_ping,2);
+            let step = 2**(Math.log2(resx)-i-1);
+            firstframe && console.log(step);
+            gl.uniform1f(loc_step_ping,step);
+            gl.uniform1f(loc_frame_ping,frame);
+            gl.bindVertexArray(vao_ping);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+					frame++
+        }
+
+
     });
 
 //init program
@@ -309,6 +444,9 @@ function main() {
  
 
    var then = 0;
+//    while (!glyphLoaded) {
+    
+//    }
     requestAnimationFrame(drawScene);
 
     function drawScene(time) {
@@ -318,50 +456,6 @@ function main() {
         var deltaTime = time - then;
         // Remember the current time for the next frame.
         then = time;
-
-        //ping
-        gl.bindFramebuffer(gl.FRAMEBUFFER, ping);
-        
-        gl.viewport(0,0,resx,resy);
-        gl.clearColor(0, 0, 1, 1);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.useProgram(initProgram);
-        gl.uniform1i(loc_glyph_init,0);
-        
-        gl.bindVertexArray(vao_ping);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-        firstframe && console.log(resx);
-        let steps = Math.ceil(Math.log2(resx));
-        firstframe && console.log("steps: "+steps);
-         steps = Math.min(steps,steps_override);
-			let frame=0
-        for (let i = 0; i <steps; i++) {
-            //pong
-            gl.bindFramebuffer(gl.FRAMEBUFFER,pong);
-            gl.viewport(0,0,resx,resy);
-            gl.clearColor(0, 0, 1, 1);
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            gl.useProgram(pongProgram);
-            gl.uniform1i(loc_ping_pong,1);
-            gl.bindVertexArray(vao_pong);
-            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-            //ping
-            gl.bindFramebuffer(gl.FRAMEBUFFER,ping);
-            gl.viewport(0,0,resx,resy);
-            gl.clearColor(0, 0, 1, 1);
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            gl.useProgram(pingProgram);
-            gl.uniform1i(loc_pong_ping,2);
-            let step = 2**(Math.log2(resx)-i-1);
-            firstframe && console.log(step);
-            gl.uniform1f(loc_step_ping,step);
-            gl.uniform1f(loc_frame_ping,frame);
-            gl.bindVertexArray(vao_ping);
-            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-					frame++
-        }
 
     
         //main canvas stuffs
